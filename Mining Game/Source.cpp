49c8b93cf28a1540e4ef04565c2bdcc2bcdfd2fc
J.J.Gallucci <jacobgalluch@gmail.com>
2024-02-02 11:18:03 -0500

    - changed window size - fixed skills placement

--- Mining Game/Source.cpp
+++ Mining Game/Source.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[])
 {
-	sf::RenderWindow window(sf::VideoMode(600.f, 400.f), "Mining Game", sf::Style::Close);
+	sf::RenderWindow window(sf::VideoMode(1200.f, 800.f), "Mining Game", sf::Style::Close);
 	GameManager manager;
 	manager.setup(&window);
 
--- Mining Game/Ui.cpp
+++ Mining Game/Ui.cpp
@@ -15,8 +15,9 @@ void Ui::Initialize() {
 		
 	}
 	skills_sprite_.setPosition(0,0);
+	skills_sprite_.setScale(.5f, .5f);
 }
 
 void Ui::Update(const sf::View& view) {
-	skills_sprite_.setPosition(view.getCenter().x + 100.f, view.getCenter().y + 20.f);	// I can redo this to be more mathematical forgor
+	skills_sprite_.setPosition(view.getCenter().x + 100.f, view.getCenter().y + 35.f);	// I can redo this to be more mathematical forgor
 }
